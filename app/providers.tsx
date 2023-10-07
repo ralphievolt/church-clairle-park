b5848e5f3a9ccc3552918a85d9db3ed14c70e0f5
ralphie <ralpheiligan@gmail.com>
2023-10-07 10:23:54 -0400

    init

---
+++ app/providers.tsx
@@ -0,0 +1,34 @@
+// app/providers.tsx
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#f0e4ff",
+      100: "#cbb2ff",
+      200: "#a480ff",
+      300: "#7a4dff",
+      400: "#641bfe",
+      500: "#5a01e5",
+      600: "#5200b3",
+      700: "#430081",
+      800: "#2d004f",
+      900: "#14001f",
+    },
+  },
+  fonts: {
+    heading: `'Inter', sans-serif`,
+    body: `'Inter', sans-serif`,
+  },
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
